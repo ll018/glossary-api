8b35d58fbdf0d51777d14af76e55ddf70c387f8a
ll018 <developing-37129aa@protonmail.com>
2023-02-05 16:55:11 +0100

    Init

---
+++ src/main/java/com/pleonasmen/glossaryapi/GlossaryApiApplication.java
@@ -0,0 +1,13 @@
+package com.pleonasmen.glossaryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlossaryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlossaryApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pleonasmen/glossaryapi/GlossaryapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pleonasmen.glossaryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlossaryapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
